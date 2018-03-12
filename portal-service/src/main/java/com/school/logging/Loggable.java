cfbe67f904fef20be82947844b8c6362633a620c
strider90arun <strider90arun@gmail.com>
2018-03-11 19:51:35 -0700

    Spring AOP based logging

---
+++ portal-service/src/main/java/com/school/logging/Loggable.java
@@ -0,0 +1,16 @@
+package com.school.logging;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Loggable {
+
+}
